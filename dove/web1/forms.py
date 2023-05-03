4760dc98690be279316d9dbee39a7bb67085f400
hthuong1308 <132075314+hthuong1308@users.noreply.github.com>
2023-05-03 20:04:32 +0700

    Thương sửa

--- dove/web1/forms.py
+++ dove/web1/forms.py
@@ -8,4 +8,9 @@ class RegistrationForm(forms.Form):
     username = forms.CharField(max_length=30)
     password = forms.CharField(max_length=30)
     ten = forms.CharField(max_length=30)
-    
+
+class Register(forms.Form):
+    name = forms.CharField(max_length=30)
+    email=forms.CharField(max_length=40)
+    password = forms.CharField(max_length=30)
+    confirm = forms.CharField(max_length=30)
